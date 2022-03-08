24c0807e017902d5e746b3266aaaff366483d273
unknown <alexadrito10@gmail.com>
2022-03-08 15:32:47 -0500

    first commit

---
+++ src/test/java/com/icesi/samaca/Taller1AlexSamaca/Taller1AlexSamacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icesi.samaca.Taller1AlexSamaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1AlexSamacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
